c5b4345b686f56b18892a81adb2c96b3dee845b4
Ani <83376857+sparkra1n@users.noreply.github.com>
2023-01-13 21:43:27 +0000

    update vent class

--- vent.cpp
+++ vent.cpp
@@ -1,38 +1,65 @@
+#include "vent.h"
+
+#include <Arduino.h>
+#include <stdio.h>
+
+
 class vent
 {
     private:
         double temp;
         double heatingTime;
-    
-    vent(double temperature, double openDuration) : temp(temperature), heatingTime(openDuration) {}
+    public:
+        vent(double temperature, double openDuration) : temp(temperature), heatingTime(openDuration) {}
 
-    void setTemp(const double& t)
-    {
-        temp = t;
-    }
+        void setTemp(const double& t)
+        {
+            temp = t;
+        }
 
-    void setOpenDuration(const double& d)
-    {
-        heatingTime = d;
-    }
+        void setOpenDuration(const double& d)
+        {
+            heatingTime = d;
+        }
 
-    double getTemp() const
-    {
-        return temp;
-    }
+        double getTemp() const
+        {
+            return temp;
+        }
 
-    double getHeatingTime() const
-    {
-        return heatingTime;
-    }
+        double getHeatingTime() const
+        {
+            return heatingTime;
+        }
 
-    void open()
-    {
+        void open()
+        {
+            
+        }
 
-    }
+        void close()
+        {
 
-    void close()
-    {
-
-    }
+        }
 };
+
+
+hw_timer_t *timerInterrupt = NULL;
+
+void IRAM_ATTR onTimer()
+{
+    digitalWrite(LED, !digitalRead(LED));
+}
+
+void setup() 
+{
+    timerInterrupt = timerBegin(0, 80, true);
+    timerAttachInterrupt(timerInterrupt, &onTimer, true);
+    timerAlarmWrite(timerInterrupt, 1000000, true);
+    timerAlarmEnable(timerInterrupt); //Just Enable
+}
+
+void loop() 
+{
+
+}
---
+++ vent.h
@@ -0,0 +1,14 @@
+class vent
+{
+    private:
+        double temp;
+        double heatingTime;
+    public:
+        vent(double temperature, double openDuration) : temp(temperature), heatingTime(openDuration) {}
+        void setTemp(const double& t);
+        void setOpenDuration(const double& d);
+        double getTemp() const;
+        double getHeatingTime() const;
+        void open();
+        void close();
+};
