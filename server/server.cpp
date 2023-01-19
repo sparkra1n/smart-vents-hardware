74ad5bb3268b426e3048dc8708e9b2a98caed8fa
Ani <83376857+sparkra1n@users.noreply.github.com>
2023-01-19 13:38:34 -0800

    Update server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -1,27 +1,55 @@
+/*
+ * File     : server.cpp
+ * Authors  : Ani
+ * Date     : 1/6/23
+*/
+
 #include "server.h"
-#include "../client/vent.h"
+
+#include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
 #include <stdio.h>
 #include <vector>
-#include <Arduino.h>
+#include <string>
+#include <array>
+
+class VentS
+{
+    private:
+        std::array<__uint8_t, 6> mac;
+        std::string room;
+    public:
+        VentS() : mac(mac), room(room) {}
+        void send() 
+        {
+
+        }
+};
 
 class Server
 {
     private: 
-        std::vector<Vent> vents;
-        const char* ssid;
-        const char pswd;
-    
+        std::vector<VentS> ventList;
     public:
-        Server() : vents(vents), ssid(ssid), pswd(pswd) {}
+        Server() : vents(ventList), ssid(ssid), pswd(pswd) {}
+        inline void addVent(const VentS& v)
+        {
+            ventList.emplace_back(v);
+        }
 
-
+        inline void removeVent(const VentS& v)
+        {
+            ventList.erase(std::remove(ventList.begin(), ventList.end(), v), ventList.end());
+        }
+        
 };
 
 int main()
 {
     while (1)
     {
-        
+
     }
 }
